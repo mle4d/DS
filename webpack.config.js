2b686e4d9425e28899e850547a87b7e40a5ff3f6
mle4d <mle4d@me.com>
2020-11-30 21:20:51 -0800

    uhhh webpack?

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+const HtmlPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+module: {
+    rules: [
+      {
+        exclude: /node_modules/
+        }
+    ]};
